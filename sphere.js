095d008c5e9b966c71e40512c9b0d9b4c94cf15a
Timothy Zhou <z.timothy96@gmail.com>
2020-12-04 12:38:15 -0800

    can render a single sphere

---
+++ sphere.js
@@ -0,0 +1,79 @@
+/**
+ * @fileoverview Sphere - A simple 3D sphere using WebGL
+ * @author Eric Shaffer
+ */
+
+/**
+ * Global mesh for unit sphere, used to generate all other spheres
+ */
+var sphereSoup = [];
+var sphereNormals = [];
+var numT= sphereFromSubdivision(6,sphereSoup,sphereNormals);
+console.log("Generated ", numT, " triangles"); 
+
+/**
+ * Similarly, shared global buffers
+ */
+var sphereVertexPositionBuffer = gl.createBuffer();
+var sphereVertexNormalBuffer = gl.createBuffer();
+
+/** Class implementing 3D sphere. */
+class Sphere{   
+/**
+ * Initialize members of a Terrain object
+ * @param {number} div Number of triangles along x axis and y axis
+ * @param {number} minX Minimum X coordinate value
+ * @param {number} maxX Maximum X coordinate value
+ * @param {number} minY Minimum Y coordinate value
+ * @param {number} maxY Maximum Y coordinate value
+ */
+    constructor(radius, position, velocity, acceleration, color){
+        this.radius = radius;
+        this.position = position;
+        this.velocity = velocity;
+        this.acceleration = acceleration;
+        this.color = color;
+    }
+    
+    /**
+    * Send the buffer objects to WebGL for rendering 
+    */
+    loadBuffers()
+    {
+        gl.bindBuffer(gl.ARRAY_BUFFER, sphereVertexPositionBuffer);      
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(sphereSoup), gl.STATIC_DRAW);
+        sphereVertexPositionBuffer.itemSize = 3;
+        sphereVertexPositionBuffer.numItems = numT*3;
+        console.log(sphereSoup.length/9);
+    
+        // Specify normals to be able to do lighting calculations
+        
+        gl.bindBuffer(gl.ARRAY_BUFFER, sphereVertexNormalBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(sphereNormals),
+                  gl.STATIC_DRAW);
+        sphereVertexNormalBuffer.itemSize = 3;
+        sphereVertexNormalBuffer.numItems = numT*3;
+    
+        console.log("Normals ", sphereNormals.length/3);   
+    }
+    
+    /**
+    * Render the triangles 
+    */
+    drawTriangles(){
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.VertexPositionBuffer);
+        gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, this.VertexPositionBuffer.itemSize, 
+                         gl.FLOAT, false, 0, 0);
+
+        // Bind normal buffer
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.VertexNormalBuffer);
+        gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, 
+        this.VertexNormalBuffer.itemSize,
+                           gl.FLOAT, false, 0, 0);   
+    
+        //Draw 
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.IndexTriBuffer);
+        gl.drawElements(gl.TRIANGLES, this.IndexTriBuffer.itemSize*this.IndexTriBuffer.numItems, gl.UNSIGNED_INT,0);
+    }
+    
+}
